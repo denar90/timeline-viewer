e34e369b75e21c6dac22f565c8b8506ed7caaaa7
Artem Denysov <denysov.artem@gmail.com>
2017-09-11 23:22:41 +0300

    test

--- test/integration/index_page_spec.js
+++ test/integration/index_page_spec.js
@@ -7,7 +7,6 @@ describe('Timeline-Viewer index.html page', () => {
       .server()
       .visitIndex();
   });
-
   context('Authorize button', () => {
     it('should be hidden before script checking auth', () => {
       cy.get('#auth').should('not.be.visible');
